edd7d6a694a0247c9c6ae2037193544c79d052a2
Katie <katharinefrechette@gmail.com>
2022-04-20 19:16:58 -0600

    updates

--- src/Character.java
+++ src/Character.java
@@ -1,58 +1,53 @@
-    import java.util.Random;
+/*
+Name: Joshua Johnson
+date: 4/20/22
+Program Name: Rat Stomp - Public Class Character
+Program Purpose: To set up, initialize, and allow user to update information for player characters and rat characters. Additonally, houses the fight class which determines the outcome of player vs. rat confrontations. 
+*/
 
-
-    public class Character {  
-
-        String name;  
-        int healthPoints; //health points
-        int attackPoints; //attack points
-        boolean alive; 
+public class Character {  
+    String name;  
+    int healthPoints; //health points
+    int attackPoints; //attack points
+    boolean alive; 
+    
+   public Character() {  // initialize private fields
+      name = "Randy"; 
+      healthPoints = 100; 
+      attackPoints = 20; 
+      alive = true;  
+   }
+   public Character(String name, int healthPoints, int attackPoints, boolean alive) { // Allows set of private fields. 
+      this.name = name; 
+      this.healthPoints = healthPoints; 
+      this.attackPoints = attackPoints; 
+      this.alive = alive;
+   }
+   public void setName(String inputName) {
+      this.name = inputName;
+   }
+    public String getName(){
+       return name; 
+   }
+    public int getHP(){
+       return healthPoints; 
+   }
+    public void setHP(int dmg){ //dmg name can be anything? Correct? 
+       this.healthPoints = this.healthPoints - dmg;
+   }
+    public int getAP(){
+       return attackPoints; 
+   }
+    public boolean isAlive() {
+       return alive; 
+   }   
+}
+           
+          
         
-    public Character() {  // initialize private fields
-        name = "Charlie"; 
-        healthPoints = 100; 
-        attackPoints = 20; 
-        alive = true;  
-    }
-    public Character(String name, int healthPoints, int attackPoints, boolean alive) { // Allows set of private fields. 
-        this.name = name; 
-        this.healthPoints = healthPoints; 
-        this.attackPoints = attackPoints; 
-        this.alive = alive;
-    }
-    public void setName(String inputName) {
-        name = inputName;
-    }
-        public String getName(){
-        return name; 
-    }
-        public int getHP(){
-        return healthPoints; 
-    }
-        public int getAP(){
-        return attackPoints; 
-    }
-        public boolean isAlive() {
-        return alive; 
-    }
-    
-        //Rat rat1 = new Rat();  
-        //Player player1 = new Player(); 
-    
-    //while(userInput > 0){  // 1.Attack 2.shoot arrow 0.run!
-        //if(userInput == 1){
-            //rat1.healthPoints = rat1.healthPoints() - player1.getattackPoints();      //initialize in main code???***
-            
-            //If(rat1.HealthPoints <= 0){
-                //userInput = 3;
-            // }
-            //else {
-                //System.out.println("1. Continue the fight."); 
-                //System.out.println("2. flee"); 
-    }          // userInput = scnr.nextInt();  
-            //}
-            
-    
-    
-    
-    
+  
+  
+  
+   
+
+
--- src/Rat.java
+++ src/Rat.java
@@ -1,3 +1,8 @@
+/*Name: Joshua Johnson
+  Date: 4/20/22
+  Program Name: Rat Stomp - Public Class Rat
+  Program date: Auto generates rat characters with trails in class Character and assigns them random health points and attack points. 
+*/
 import java.util.Random;  
 public class Rat extends Character {
     
@@ -13,3 +18,5 @@ public class Rat extends Character {
        rat.attackPoints = 1 + dice.nextInt(20); 
     }
 }
+ 
+ 
